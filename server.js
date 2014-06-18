4c7d2040b0bba84258192f3e6c91028a22665d1c
Samantha Jacobo <jacobovideography@gmail.com>
2014-06-18 15:16:47 -0500

    added remove, create and update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var jf = require("jsonfile");
 
 console.log(__dirname)
 app.use(express.static(__dirname + '/client'));
@@ -45,7 +46,48 @@ app.get ('/items/:id', function (req, res){
     res.send(404)
   }); 
 });
+
+app.delete('/item/:id', function (req,res) {
+  fs.readFile('data.json', function(err, data) {
+    if (err) throw err;
+    var dataitems=JSON.parse (data.toString());
+    var newdata = [];
+    for (var i = 0; i < dataitems.length; i++) {
+      if (dataitems[i].id === req.params.id) {
+      }
+      else {
+        newdata.push(dataitems[i]);
+      }
+    }
+    jf.writeFileSync(newdata,'data.json'); 
+    res.send(200);
+  }); 
+});
+
+app.create('/item/', function (req,res) {
+  fs.readFile('data.json', function(err, data) {
+    if (err) throw err;
+    var dataitems=JSON.parse (data.toString());
+    dataitems.push(req.params);
+    jf.writeFileSync(dataitems,'data.json');
+    res.send(200);
+  }); 
+});
  
+ app.update ('/items/:id', function (req, res){
+  fs.readFile('data.json', function(err, data) {
+    if (err) throw err;
+    var dataitems=JSON.parse (data.toString());
+    for (var i = 0; i < dataitems.length; i++) {
+      if (dataitems[i].id === req.params.id) {
+        dataitems[i] = req.params;  
+      }
+    };
+    jf.writeFileSync(dataitems,"data.json"); 
+    res.send(200);
+  }); 
+});
+
 console.log("Starting to listen");
 
 var server = app.listen(3000, function() {
